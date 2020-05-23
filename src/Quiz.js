05f82577987534be570b17e5457b221dae3875ca
Ole Kristian Losvik <ole@losol.no>
2020-05-23 22:18:25 +0200

    m7

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,12 +15,16 @@ class Quiz extends Component {
     })
 
   }
+  handleResetClick() {
+    this.state = { quiz_position: 1 }
+  }
   render() {
     const isQuizEnd = (this.state.quiz_position - 1) == quizData.quiz_questions.length
 
     if (isQuizEnd) {
       return <div>
-        <QuizEnd />
+        <QuizEnd
+          resetClickHandler={this.handleResetClick.bind(this)} />
       </div>
     }
     else {
